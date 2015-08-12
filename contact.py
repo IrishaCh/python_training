e721ab547be9faa98173bd55ea82b7dc4fc0c4f2
Irina.Chegodaeva <irina.chegodaeva@billing.ru>
2015-08-12 13:11:28 +0300

    Задание 3: Записан тест для создания нового контакта, выделены вспомогательные методы, создан вспомогательный класс.

---
+++ contact.py
@@ -0,0 +1,28 @@
+__author__ = 'Irina.Chegodaeva'
+
+
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nickname, pic, title, company_name, company_address,
+                 home_phone, mobile_phone, work_phone, fax, email_1, email_2, email_3, homepage,
+                 birth_year, anniv_year, home_addr, notes, extra_phone):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.pic = pic
+        self.title = title
+        self.company_name = company_name
+        self.company_address = company_address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.birth_year = birth_year
+        self.anniv_year = anniv_year
+        self.home_addr = home_addr
+        self.notes = notes
+        self.extra_phone = extra_phone
