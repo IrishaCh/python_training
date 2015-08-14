03e0aa58f6ed18ee2f12396c47548e209549ee0d
Irina.Chegodaeva <irina.chegodaeva@billing.ru>
2015-08-14 09:27:58 +0300

    Выделены классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,7 +14,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.fill_contacts_form(Contact(first_name="First_Name",
+    app.contact.fill_form(Contact(first_name="First_Name",
                                         middle_name="Middle_Name",
                                         last_name="Last_Name",
                                         nickname="Nickname",
@@ -40,13 +40,13 @@ def test_add_contact(app):
     dataset = (("1", "3"), ("2", "2"), ("3", "12"), ("4", "3"), ("5", "1"))
     for i in range(0, 5):
         (combo, value) = dataset[i]
-        app.choose_from_combo(combo, value, i)   # 1 - комбо, 3  - 1 число
+        app.contact.choose_from_combo(combo, value, i)   # 1 - комбо, 3  - 1 число
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.fill_contacts_form(Contact(first_name="",
+    app.contact.fill_form(Contact(first_name="",
                                         middle_name="",
                                         last_name="",
                                         nickname="",
@@ -69,5 +69,5 @@ def test_add_empty_contact(app):
                                         extra_phone=""))
     # fill combo-boxes: combo - № комбобокса, value - значение (1 - соответствует "не выбрано")
     for i in range(0, 5):
-        app.choose_from_combo(combo=str(i+1), value="1", step_counter=i)   # 1 - комбо, 1  - число не выбрано
+        app.contact.choose_from_combo(combo=str(i+1), value="1", step_counter=i)   # 1 - комбо, 1  - число не выбрано
     app.session.logout()
