c4a2854b6aa15226bb4a8a8c291e799839fb683f
Irina.Chegodaeva <irina.chegodaeva@billing.ru>
2015-09-07 15:04:34 +0300

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 __author__ = 'Irina.Chegodaeva'
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,12 +7,20 @@ from fixture.libs import CommonLib
 
 
 class Application:
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.libs = CommonLib(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -26,7 +34,7 @@ class Application:
         if wd.current_url.endswith("addressbook/") and wd.find_element_by_link_text("Logout").is_displayed():
             wd.find_element_by_link_text("Logout").click()
         if not (wd.current_url.endswith("addressbook/") and wd.find_element_by_link_text("Create account").is_displayed()):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
--- test/test_contact.py
+++ test/test_contact.py
@@ -1,31 +1,22 @@
 __author__ = 'Irina.Chegodaeva'
-from model.contact import Contact
 import re
 from random import randrange
-import pytest
+from model.contact import Contact
 
 
-#def count_contact(self, app):
-#    self.app = app
-#    return app.contact.count()
-
-
-#index = count_contact()
-#@pytest.mark.parametrize("index", count_contact(), ids=[x for x in range(index)])
 def test_contact_on_home_page(app):
     if app.contact.count() == 0:
         app.contact.add_contact(Contact(first_name="test" + app.libs.substring),
                                 delete_photo=False,
                                 dataset=(("1", "3"), ("2", "2"), ("3", "12"), ("4", "3"), ("5", "1")))
-    # !!!
-    for index in range(app.contact.count()):
-        contact_from_home_page = app.contact.get_contact_list()[index]
-        contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-        assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-        assert contact_from_home_page.company_address == trim_spaces(contact_from_edit_page.company_address)
-        assert contact_from_home_page.homepage == trim_spaces(contact_from_edit_page.homepage)
-        assert contact_from_home_page.company_url == contact_from_edit_page.company_url
-        assert contact_from_home_page.all_e_mails_from_home_page == merge_e_mails_like_on_home_page(contact_from_edit_page)
+    index = randrange(app.contact.count())
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.company_address == trim_spaces(contact_from_edit_page.company_address)
+    assert contact_from_home_page.homepage == trim_spaces(contact_from_edit_page.homepage)
+    assert contact_from_home_page.company_url == contact_from_edit_page.company_url
+    assert contact_from_home_page.all_e_mails_from_home_page == merge_e_mails_like_on_home_page(contact_from_edit_page)
 
 
 def extract_numbers(phone_number):
