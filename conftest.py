6a3b6a45891e881a8093bcf94ab3aab836b094a1
Irina.Chegodaeva <irina.chegodaeva@billing.ru>
2015-09-07 15:14:45 +0300

    fix: Задание 16

--- conftest.py
+++ conftest.py
@@ -12,12 +12,14 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
     if fixture is None:
         fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username=username, password=password)
     return fixture
 
 
@@ -33,3 +35,5 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--username", action="store", default="user")
+    parser.addoption("--password", action="store", default="user")
