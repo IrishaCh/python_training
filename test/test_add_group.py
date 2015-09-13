709ffbfca89a34019f229280851771a1c67ebe63
Irina.Chegodaeva <irina.chegodaeva@billing.ru>
2015-09-13 09:24:59 +0300

    Переделываем тесты: проверяем данные, загруженные из базы данных

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,14 +3,21 @@ from model.group import Group
 import re
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
-    group.name = trim_space(group.name)
+    #group.name = trim_space(group.name)
+    # def clean(group):
+    #     return Group(id=group.id, name=group.name.strip())
+    #k = clean(new_groups)
+    # l = sorted(old_groups, key=Group.id_or_max)
+    # r = sorted(map(clean, new_groups), key=Group.id_or_max)
+    # s = 0
+    # if l != r:
+    #     s= 1
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
